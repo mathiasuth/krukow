af427765c8382d437ed8f21cdeb72e8369b72e14
Michael Lund <michael@webcloud.dk>
2014-06-17 21:39:50 +0200

    removed credit from footer

--- footer.php
+++ footer.php
@@ -11,17 +11,11 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer container row" role="contentinfo">
-	<?php if ( of_get_option('credit1', true) == 0 ) { ?>
 		<div class="site-info col-md-5">
-			<?php do_action( 'blain_credits' ); ?>
-			<?php printf( __( 'Blain Theme by %1$s.', 'blain' ), '<a href="http://inkhive.com/" rel="designer">InkHive</a>' ); ?>
-		</div><!-- .site-info -->
-	<?php } ?>	
-		<div id="footertext" class="col-md-7">
         	<?php
 			if ( (function_exists( 'of_get_option' ) && (of_get_option('footertext2', true) != 1) ) ) {
 			 	echo of_get_option('footertext2', true); } ?>
-        </div>   
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 

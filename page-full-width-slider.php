e46d04cdc9f9ab021316adaff4284d1f8a621e61
Michael Lund <michael@webcloud.dk>
2014-08-18 18:42:57 +0200

    Added link on slideshow image

--- page-full-width-slider.php
+++ page-full-width-slider.php
@@ -17,7 +17,7 @@ get_header(); ?>
 		  		for ($i=1;$i<6;$i++) {
 		  			$caption = ((of_get_option('slidetitle'.$i, true)=="")?"":"#caption_".$i);
 					if ( of_get_option('slide'.$i, true) != "" ) {
-						echo "<img src='".of_get_option('slide'.$i, true)."' title='".$caption."'>"; 
+						echo "<a href='".of_get_option('slideurl'.$i, true)."'><img src='".of_get_option('slide'.$i, true)."' title='".$caption."'></a>"; 
 						$slider_flag = true;
 					}
 				}

656066e40fe035a656055de2f71e279e63e4f478
Michael Lund <michael@webcloud.dk>
2014-08-18 19:27:56 +0200

    Added video to frontpage

--- page-full-width.php
+++ page-full-width.php
@@ -7,10 +7,12 @@ get_header(); ?>
 
 	<div id="primary" class="full-width content-area .col-md-12">
 		<main id="main" class="site-main" role="main">
-
-
+<?php if ($post->ID == "14"): ?>
+	<div style="padding-left:30px;">
+<iframe width="853" height="480" src="//www.youtube.com/embed/EsUzI9lZMak" frameborder="0" allowfullscreen></iframe>
+</div>
+<?php endif; ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
 <?php if (has_post_thumbnail( $post->ID ) ): ?>
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 <div class="featured-image"><img src="<?php echo $image[0]; ?>"></div>
